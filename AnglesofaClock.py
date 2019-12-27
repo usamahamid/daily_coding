be0a39bc8c727159354572dd810da8f96ce03558
Usama Hamid <usamahamid.amjadali@impigertech.com>
2019-12-27 09:47:09 +0530

    Solved Angles of a Clock

--- AnglesofaClock.py
+++ AnglesofaClock.py
@@ -1,6 +1,14 @@
 # Given a time in the format of hour and minute, calculate the angle of the hour and minute hand on a clock.
+import math
+
+
 def calc_angle(h, m):
-    pass
+    h = h + (m / 60)
+    hour_angle = h * 30
+    if hour_angle >= 360:
+        hour_angle = hour_angle - 360
+    minute_angle = m * 6
+    return math.fabs(hour_angle - minute_angle)
 
 
 if __name__ == '__main__':
