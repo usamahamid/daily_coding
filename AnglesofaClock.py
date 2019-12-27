bb67f86d234ae092cb7a335508f06c916fd6ad61
Usama Hamid <usamahamid.amjadali@impigertech.com>
2019-12-27 09:46:50 +0530

    Introduced Angles of a Clock

---
+++ AnglesofaClock.py
@@ -0,0 +1,12 @@
+# Given a time in the format of hour and minute, calculate the angle of the hour and minute hand on a clock.
+def calc_angle(h, m):
+    pass
+
+
+if __name__ == '__main__':
+    print(calc_angle(3, 30))  # 75
+    print(calc_angle(12, 30))  # 165
+    print(calc_angle(5, 10))  # 95
+    print(calc_angle(6, 00))  # 180
+    print(calc_angle(9, 00))  # 270
+    print(calc_angle(1, 50))  # 245
